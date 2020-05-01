65d5d4472f45ac1e27c84ac719bf62ca697011c2
ali_ele804 <frogert835@gmail.com>
2020-05-01 20:15:15 +1000

    New Build
    New Build

--- AutoTyper.py
+++ AutoTyper.py
@@ -2,6 +2,8 @@ import pyautogui
 import win32com.client as comclt
 import time
 
+# im dying inside
+
 __title__ = """
     _         _      _____
    / \  _   _| |_ __|_   __   _ _ __   ___ _ __
@@ -17,17 +19,25 @@ print("NOTE! Failsafe will be triggered if put the cursor on a corner on screen.
 
 print("To start i need info:")
 
-sentence = input("What would you like the sentence to be? ")
+sentence = input("What would you like the sentence to be?: ")
 
-_time_ = input("How many lines would you like it to create? ")
+_time_ = input("How many lines would you like it to create? (0 for infinite): ")
 
 _time_ = int(_time_) -1
 
-delay = input("How long do you want it to delay? (for applications that block spamming like Discord, 0 for none) ")
+delay = input("How long do you want it to delay? (for applications that block spamming like Discord, 0 for none): ")
 
 delay = int(delay)
 
 input("Once this starts it will begin typing. Before you start go to the application and choose where you want it to type. Press enter to begin.")
+
+if _time_ == -1:
+    while(1 < 2):
+        pyautogui.typewrite(sentence)
+
+        time.sleep(delay)
+
+        pyautogui.press("enter")
 
 for i in range(_time_):
 
